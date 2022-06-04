4683e7669259d06fb01beab5fbc9d5fc3273b5d8
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-06-04 08:44:25 -0300

    upgrade context-menu

--- buttons/menu/context-menu/animation.js
+++ buttons/menu/context-menu/animation.js
@@ -3,7 +3,13 @@ const context = document.querySelector(".context-menu")
 document.addEventListener("click", function(event) {
     var x = event.clientX;
     var y = event.clientY;
-    
+
+    var screenWidth = screen.width;
+    var screenHeight = screen.height;
+
+    x = x + 280 > screenWidth ? x - 280 : x
+    y = y + 220 > screenHeight ? y - 220 : y
+
     if (context.style.visibility === 'visible'){
         context.style.visibility = 'hidden'
     } else {
